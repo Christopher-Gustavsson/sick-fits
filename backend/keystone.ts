72a856ab014251dc3638ad7fa4028fbfd0d0deb1
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2021-03-28 15:34:51 -0700

    (backend): add authentication with keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
 import { User } from './schemas/User';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -10,7 +12,17 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET
 };
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add initial roles here
+  }
+});
+
+export default withAuth(config({
   server: {
     cors: {
       origin: [process.env.FRONTEND_URL],
@@ -27,8 +39,14 @@ export default config({
     User
   }),
   ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-    // TODO: add session values here
-  }
-});
+    // show the UI only for people who pass this test
+    isAccessAllowed: ({ session }) => {
+      // console.log(session);
+      return !!session?.data;
+    },
+  },
+  session: withItemData(statelessSessions(sessionConfig), {
+    // GraphQL query
+    User: `id name email`
+  })
+}));
